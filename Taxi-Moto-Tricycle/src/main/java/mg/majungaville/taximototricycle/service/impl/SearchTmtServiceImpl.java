c186bb330d4db8bfd2f73e5931f9a1f1dcc67aed
Andriantomanga <nabil.arrowbase@gmail.com>
2017-11-01 15:01:30 +0100

    adding services

--- Taxi-Moto-Tricycle/src/main/java/mg/majungaville/taximototricycle/service/impl/SearchTmtServiceImpl.java
+++ Taxi-Moto-Tricycle/src/main/java/mg/majungaville/taximototricycle/service/impl/SearchTmtServiceImpl.java
@@ -1,3 +1,22 @@
+
+/*
+ * Licensed to Taxi-Moto-Tricycle under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Taxi-Moto-Tricycle licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package mg.majungaville.taximototricycle.service.impl;
 
 import org.springframework.stereotype.Service;
@@ -17,6 +36,7 @@ public class SearchTmtServiceImpl implements SearchTmtService {
 
 		// appel web services sur le serveur de majungaville.mg
 		if("xyz".equals(value)) {
+			
 			tmt.setNumber(111);
 			tmt.setImmatricuation("1212MB");
 			tmt.setPhoneNumber("0323343322");
